8792575892c8dc0f5b4a40d9fbaab6ee02af713d
priyadharshini755 <priyadharshini102002@gmail.com>
2025-08-09 01:16:05 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// FAQ Toggle Functionality
+document.querySelectorAll('.faq-question').forEach(question => {
+    question.addEventListener('click', () => {
+        const answer = question.nextElementSibling;
+        const icon = question.querySelector('.faq-icon');
+
+        // Toggle answer visibility
+        answer.classList.toggle('show');
+
+        // Change icon
+        if (answer.classList.contains('show')) {
+            icon.classList.remove('fa-plus');
+            icon.classList.add('fa-times');
+        } else {
+            icon.classList.remove('fa-times');
+            icon.classList.add('fa-plus');
+        }
+    });
+});
